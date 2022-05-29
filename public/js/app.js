df04efe12197244bd8606e24897db0791d6fd438
mikemiller97 <mike.miller.42797@gmail.com>
2022-05-29 07:02:41 -0500

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ const messageTwo = document.querySelector('#message-2')
     messageOne.textContent = ''
     messageTwo.textContent = 'Loading...'
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             console.log('Error: city not found')
